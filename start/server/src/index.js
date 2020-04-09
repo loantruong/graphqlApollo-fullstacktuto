0947eebe50a1e0da6aedc136c3e961e623b6f85f
loan <loancrea@gmail.com>
2020-04-09 19:20:11 +0200

    add data sources, pagination resolvers

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,8 +1,23 @@
-const { ApolloServer } = require("apollo-server");
-const typeDefs = require('./schema')
 
-const server = new ApolloServer({typeDefs});
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
 
-server.listen().then(({url}) => {
-  console.log(`server ready at ${url}`)
-})
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -4,7 +4,7 @@ const { gql } = require('apollo-server');
 const typeDefs = gql`
 
   type Launch {
-    id: ID! 
+    id: ID!
     site: String
     mission: Mission
     rocket: Rocket
@@ -34,15 +34,34 @@ const typeDefs = gql`
   }
 
   type Query {
-    launches: [Launch]!
-    launch(id: ID!): Launch #return a single Launch that correspondes to id argument
-    me: User
-  }
+  launches( # replace the current launches query with this one.
+    """
+    The number of results to show. Must be >= 1. Default = 20
+    """
+    pageSize: Int
+    """
+    If you add a cursor here, it will only return results _after_ this cursor
+    """
+    after: String
+  ): LaunchConnection!
+  launch(id: ID!): Launch
+  me: User
+}
 
+"""
+Simple wrapper around our list of launches that contains a cursor to the
+last item in the list. Pass this cursor to the launches query to fetch results
+after these.
+"""
+type LaunchConnection { # add this below the Query type as an additional type.
+  cursor: String!
+  hasMore: Boolean!
+  launches: [Launch]!
+}
   type Mutation {
     bookTrips(launchIds: [ID]!): TripUpdateResponse!
     cancelTrip(launchId: ID!): TripUpdateResponse!
-    login(email: String): String #login token
+    login(email: String): String # login token
   }
 
   #TripUpdateResponse must contains: 
