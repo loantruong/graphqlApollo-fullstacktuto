b51c3e838fd12d626b6b6b2853aad757f834f4a1
loan <loancrea@gmail.com>
2020-04-13 12:42:26 +0200

    add item card, add card

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -2,43 +2,50 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import gql from "graphql-tag";
-import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
 import React from "react";
 import ReactDOM from "react-dom";
-import Page from "./pages";
+import Login from "./pages/login";
 import injectStyles from "./styles";
 import Pages from "./pages";
+import { resolvers, typeDefs } from "./resolvers";
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: "http://localhost:4000/",
-});
-
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
-  link,
+  link: new HttpLink({
+    uri: "http://localhost:4000/graphql",
+    headers: {
+      authorization: localStorage.getItem("token"),
+    },
+  }),
+
+  typeDefs,
+  resolvers,
 });
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem("token"),
+    cartItems: [],
+  },
+});
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById("root")
 );
-
-// ... above is the instantiation of the client object.
-/* client
-  .query({
-    query: gql`
-      query GetLaunch {
-        launch(id: 56) {
-          id
-          mission {
-            name
-          }
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result)); */
--- start/client/src/pages/launch.tsx
+++ start/client/src/pages/launch.tsx
@@ -2,31 +2,26 @@ import React, { Fragment } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
+import { LAUNCH_TILE_DATA } from "./launches";
 import { Loading, Header, LaunchDetail } from "../components";
 import { ActionButton } from "../containers";
 import { RouteComponentProps } from "@reach/router";
 import * as LaunchDetailsTypes from "./__generated__/LaunchDetails";
 
-import { LAUNCH_TILE_DATA } from "./launches";
-
 export const GET_LAUNCH_DETAILS = gql`
   query LaunchDetails($launchId: ID!) {
     launch(id: $launchId) {
-      id
+      isInCart @client
       site
-      isBooked
       rocket {
-        id
-        name
         type
       }
-      mission {
-        name
-        missionPatch
-      }
+      ...LaunchTile
     }
   }
+  ${LAUNCH_TILE_DATA}
 `;
+
 interface LaunchProps extends RouteComponentProps {
   launchId?: any;
 }
